ac7c8902e91c16c56df63c00513992ccbe20d0ff
codewithdolly <intensedolly321@gmail.com>
2024-06-06 02:23:52 +0530

    added drag and drop feature and add node by clicking btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,16 @@
-import React, { useCallback } from 'react';
-import ReactFlow, {
-  MiniMap,
-  Controls,
-  Background,
-  useNodesState,
-  useEdgesState,
-  addEdge,
-  Panel,
-} from 'reactflow';
- 
-import 'reactflow/dist/style.css';
-import Flow from './Componentes/Flow/Flow';
- 
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
- 
+import React, { useCallback } from "react";
+
+import "reactflow/dist/style.css";
+import AddNodeAndSave from "./Componentes/AddNodeAndSave/AddNodeAndSave";
+import DragandDrop from "./Componentes/DragandDrop/DragandDrop";
+
 export default function App() {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
- 
-  const onConnect = useCallback(
-    (params) => setEdges((eds) => addEdge(params, eds)),
-    [setEdges],
-  );
- 
+
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      {/* <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-      >
-        <Controls />
-        <MiniMap />
-        <Background variant="dots" gap={12} size={1} />
-        <Panel /> */}
-        {/* <Flow />
-      </ReactFlow> */}
-<Flow />
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <AddNodeAndSave />
+      {/* <DragandDrop /> */}
+      
     </div>
   );
 }
